be40594e8b8081de69983aa2678fec1641c2b50d
Hankaji <hankaji135@gmail.com>
2024-03-18 10:52:45 +0700

    Remove unused CSS and update layout and dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { Providers } from "./providers";
-// import "@styles/globals.css";
+import "@styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +16,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className="dark`">
+		<html lang="en" className="light">
 			<body className={inter.className}>
 				<Providers>{children}</Providers>
 			</body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@nextui-org/button";
 import Image from "next/image";
+import Test from "./test";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center gap-4 p-24">
+		<main
+			className="flex 
+                min-h-screen flex-col 
+                items-center gap-4 p-24"
+		>
 			<Button color="danger">TESTING</Button>
+
+			<Image src="/vercel.svg" alt="logo" width={200} height={200} />
 		</main>
 	);
 }
