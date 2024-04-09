14969eb986fd687a86e9bdb6d38fb625c52007ba
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-04-09 15:05:25 +0700

    Update color pallete

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,11 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google"
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+import { Montserrat as FontSans, Fira_Code as FontMono } from "next/font/google";
 
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+})
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
 })
--- config/theme.ts
+++ config/theme.ts
@@ -8,8 +8,57 @@ export const themeConfig = {
     themes: {
         light: {
             colors: {
-                test: "#ff0000",
-            }
+                background: {
+                    DEFAULT: "#F9FAFB",
+                    modal: "#979797",
+                },
+                text: {
+                    black: "#000000",
+                    gray: "#EBEBEB",
+                },
+                primary: {
+                    yellow: {
+                        50: "#FFF9E6",
+                        100: "#FDF8E2",
+                        200: "#FBF0C6",
+                        300: "#F4E1A7",
+                        400: "#EAD08D",
+                        500: "#DCB968", // primary
+                        600: "#BD974C",
+                        700: "#9E7734",
+                        800: "#7F5A21",
+                        900: "#694513",
+                        DEFAULT: "#DCB968",
+                    },
+                    blue: {
+                        50: "#E9F0FF",
+                        100: "#DCDFF7",
+                        200: "#BBC0EE",
+                        300: "#8C92CF",
+                        400: "#5E649F",
+                        500: "#2C305F", // primary
+                        600: "#202351",
+                        700: "#161944",
+                        800: "#0E1037",
+                        900: "#080A2D",
+                        DEFAULT: "#2C305F",
+                    },
+                },
+                secondary: {
+                    yellow: "#F8DA92",
+                    blue: "#97ABD6",
+                },
+                supporting: {
+                    red: "#BB2649",
+                    pink: "#EDCDC2",
+                    lightpink: "#D4CACD",
+                }
+            },
+            layout: {
+                radius: {
+                    DEFAULT: "16px",
+                }, 
+            },    
         },
         dark: {
             colors: {
