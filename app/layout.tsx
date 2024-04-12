67862dec228f4968c87c4e1fa864a69b64f11026
Hankaji <hankaji135@gmail.com>
2024-04-12 15:47:19 +0700

    Finished IntroSection wireframe

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,9 +46,9 @@ export default function RootLayout({
 				<Providers
 					themeProps={{ attribute: "class", defaultTheme: "light" }}
 				>
-					<div className="relative flex flex-col h-screen">
+					<div className="relative items-center flex flex-col h-screen">
 						{/* <Navbar /> */}
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+						<main className="container min-w-full flex-grow px-side-margin">
 							{children}
 						</main>
 						<footer className="w-full flex items-center justify-center py-3">
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,17 @@ import { Button } from "@nextui-org/button";
 import Image from "next/image";
 
 import React from "react";
+import IntroSection from "./components/intro";
 
 const Home = () => {
 	return (
-		<main
+		<div
 			className="flex
                 min-h-screen flex-col 
-                items-center gap-4 p-24"
+                items-center gap-4"
 		>
-			<p className="text-tailwind text-ft-heading">Placeholder</p>
-		</main>
+			<IntroSection />
+		</div>
 	);
 };
 
