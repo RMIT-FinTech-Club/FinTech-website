0f11f8e296ac522ff3732a00f849e67b36a5ef9b
Duydao28102004 <duybaodao28102005@gmail.com>
2024-04-15 09:58:58 +0700

    add logo and icon in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fontSans } from "@/config/fonts";
+import { Mail, BrandFacebook, BrandInstagram, BrandTiktok  } from "tabler-icons-react";
 
 const Footer = () => {
     return (
@@ -7,41 +7,36 @@ const Footer = () => {
             <div className="mx-auto w-[1652px] h-[350px] mt-[62px] flex">
                 <div className="w-1/3 mr-[32px]">
                     <div className="mt-[20px]">
-                        <h1 className="text-ft-text-bright text-4xl font-semibold fontSans">About Us</h1>
-                        <p className="mt-[4px] mr-[32px] text-ft-text-bright">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut bibendum lacus. Etiam eleifend nisl mi, vitae dignissim lectus elementum eu. </p>
+                        <h1 className="text-ft-text-bright text-[36px] font-semibold">About Us</h1>
+                        <p className="mt-[4px] mr-[32px] text-ft-text-bright text-[18px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut bibendum lacus. Etiam eleifend nisl mi, vitae dignissim lectus elementum eu. </p>
+                        <img className="w-[125px] h-[125px] mt-[20px]" src="/whiteLogo.png" alt="White logo" />
                     </div>
                 </div>
                 <div className="w-1/3 mr-[32px]">
                     <div className="mt-[20px]">
-                        <h1 className="text-ft-text-bright text-4xl font-semibold">Important Links</h1>
-                        <p className="mt-[4px] text-ft-text-bright">Home</p>
-                        <p className="mt-[4px] text-ft-text-bright">About Us</p>
-                        <p className="mt-[4px] text-ft-text-bright">Events</p>
-                        <p className="mt-[4px] text-ft-text-bright">Projects</p>
-                        <p className="mt-[4px] text-ft-text-bright">Join Us</p>
+                        <h1 className="text-ft-text-bright text-[36px] font-semibold">Important Links</h1>
+                        <p className="mt-[4px] text-ft-text-bright text-[18px]">Home</p>
+                        <p className="mt-[4px] text-ft-text-bright text-[18px]">About Us</p>
+                        <p className="mt-[4px] text-ft-text-bright text-[18px]">Events</p>
+                        <p className="mt-[4px] text-ft-text-bright text-[18px]">Projects</p>
+                        <p className="mt-[4px] text-ft-text-bright text-[18px]">Join Us</p>
                     </div>
                 </div>
                 <div className="w-1/3 mr-[32px]">
                     <div className="mt-[20px]">
-                        <h1 className="ft-heading-1 text-ft-text-bright text-4xl font-semibold">Contact Info</h1>
+                        <h1 className="ft-heading-1 text-ft-text-bright text-[36px] font-semibold">Contact Info</h1>
                         <div className="flex items-center mt-[12px]">
-                            <p className="text-ft-text-bright">rmitfintechclub.sgs@rmit.edu.vn</p>
-                        </div>
-                        <div className="flex items-center mt-[12px]">
-                            <p className="text-ft-text-bright">rmitfintechclub.sgs@rmit.edu.vn</p>
-                        </div>
-                        <div className="flex items-center mt-[12px]">
-                            <p className="text-ft-text-bright">rmitfintechclub.sgs@rmit.edu.vn</p>
-                        </div>
-                        <div className="flex items-center mt-[12px]">
-                            <p className="text-ft-text-bright">rmitfintechclub.sgs@rmit.edu.vn</p>
+                            <Mail className="w-[30px] h-[30px] text-ft-text-bright mr-[16px]"/>                                                                  
+                            <BrandFacebook className="w-[30px] h-[30px] text-ft-text-bright mr-[16px]"/>                                                  
+                            <BrandInstagram className="w-[30px] h-[30px] text-ft-text-bright mr-[16px]"/>                                      
+                            <BrandTiktok className="w-[30px] h-[30px] text-ft-text-bright mr-[16px]"/>                           
                         </div>
                     </div>
                 </div>
             </div>
 			<div className="mx-auto w-[1652px] h-[3px] bg-ft-secondary-yellow rounded-full"></div>
 			<div className="mx-auto my-[13px]">
-			    <p className="text-ft-text-bright">Copyright ©2024 All rights reserved | This website is designed and built by RMIT FinTech Club </p>
+			    <p className="text-ft-text-bright text-[18px]">Copyright ©2024 All rights reserved | This website is designed and built by RMIT FinTech Club </p>
 			</div>
         </footer>
     )
