e9047856c80c8cbfeb009b6ee867e94317f220d5
GuitarHero2004 <anhminhnts2004@gmail.com>
2024-04-16 16:19:21 +0700

    add <Members /> to test the functionalities (only the skeleton first - no decoration applied yet)

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 import { Button } from "@nextui-org/button";
 import Image from "next/image";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 import React from "react";
+import Members from "./about/members";
 import IntroSection from "./components/intro";
 
 const Home = () => {
 	return (
-		<div
-			className="flex
-                min-h-screen flex-col 
-                items-center gap-4"
-		>
-			<IntroSection />
-		</div>
+		// <Router>
+			<div className="flex min-h-screen flex-col items-center gap-4">
+				{/* <IntroSection />  */}
+				<Members />
+			</div>
+		// </Router>
 	);
 };
 
