c3ee3fe86ecc92063f8efcaa90131667629b97b1
Duydao28102004 <119027061+Duydao28102004@users.noreply.github.com>
2024-04-17 14:47:13 +0700

    center the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <footer className="md:h-fit w-full bg-ft-primary-blue flex flex-col">
-            <div className="mx-auto md:w-fit h-fit mt-[62px] mb-[20px] flex flex-col md:flex-row">
+            <div className="2xl:mx-auto mx-[30px] md:w-fit h-fit mt-[62px] mb-[20px] flex flex-col md:flex-row">
                 <div className="md:w-1/3 mr-[32px] w-full">
                     <div className="mt-[20px] flex flex-col-reverse items-center md:block">
                         <div className="flex flex-col items-center md:block mb-[40px] md:mb-0 w-[90%] md:w-full">
@@ -25,7 +25,7 @@ const Footer = () => {
                         <li><a href="#" className="block"><p className="text-ft-text-bright">Join Us</p></a></li>
                     </ul>
                 </div>
-                <div className="md:w-1/3 mr-[32px] w-full mb-[40px] md:mb-0">
+                <div className="md:w-1/3 w-full mb-[40px] md:mb-0">
                     <div className="mt-[20px] flex items-center flex-col md:block">
                         <h5 className="ft-heading-1 text-ft-text-bright">Contact Info</h5>
                         <div className="flex items-center mt-[12px]">
