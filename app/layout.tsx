aae651743e29a04ce8ed4ffda1d7169dd9a2fccb
Logan <phamphuocsang5850@gmail.com>
2024-04-18 22:22:44 +0700

    add responsive for 2k resolution

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
 				<Providers
 					themeProps={{ attribute: "class", defaultTheme: "light" }}
 				>
-					<div className="relative flex flex-col h-screen">
+					<div className="relative flex flex-col h-screen overflow-x-hidden">
 						{/* <Navbar /> */}
 						<main className="container mx-auto max-w-7xl p-8 px-6 flex-grow">
 							{children}
