123adcc7dfd502e47c0da662274669268a2813e4
Hankaji <hankaji135@gmail.com>
2024-04-18 20:53:53 +0700

    Fixed weird dot between classes. Used explicit type instead of any

--- app/about/vision-mission/puzzleItem.tsx
+++ app/about/vision-mission/puzzleItem.tsx
@@ -1,10 +1,19 @@
-const PuzzleItem = ({ bgColor, circleColor, borderRadius, circlePosition }) => {
+import React from "react";
+
+type PuzzleItemProps = {
+    bgColor: string;
+    circleColor: string;
+    borderRadius: string;
+    circlePosition: string;
+};
+
+const PuzzleItem: React.FC<PuzzleItemProps> = ({ bgColor, circleColor, borderRadius, circlePosition }) => {
 	return (
 		<div
-			className={`w-24·h-48·z-10·md:right-[100%]·md:top-24·md:rotate-0·${bgColor}·${borderRadius}`}
+			className={`w-24 h-48 z-10 md:right-[100%] md:top-24 md:rotate-0 ${bgColor} ${borderRadius}`}
 		>
 			<div
-				className={`absolute·w-3·h-3·rounded-full·${circleColor}·${circlePosition}`}
+				className={`absolute w-3 h-3 rounded-full ${circleColor} ${circlePosition}`}
 			/>
 		</div>
 	);
