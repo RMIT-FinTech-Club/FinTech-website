b0afdd064736f0d1c828a6a4a29e2486334cba9b
Hankaji <hankaji135@gmail.com>
2024-04-18 17:20:25 +0700

    Bun check

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
 import { fontSans } from "@/config/fonts";
 import { siteConfig } from "@/config/site";
 import { Link } from "@nextui-org/link";
