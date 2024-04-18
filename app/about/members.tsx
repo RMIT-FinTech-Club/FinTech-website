2863b5370ab086c8e71ed18465d5f832b7ba687e
GuitarHero2004 <anhminhnts2004@gmail.com>
2024-04-18 22:14:59 +0700

    update overflow-x-hidden for xl:  size

--- app/about/members.tsx
+++ app/about/members.tsx
@@ -209,30 +209,35 @@ const Members: React.FC = () => {
 	};
 
 	// useEffect(() => {
-	// 	document.body.style.maxWidth = "1920px";
-	// 	document.body.style.overflowX = "hidden";
-	// 	window.addEventListener("resize", () => {
-	// 		if (window.innerWidth === 1920 && window.innerHeight === 1080) {
+	// 	const resizeHandler = () => {
+	// 		if (window.innerWidth >= 1920) {
 	// 			document.body.style.overflowX = "hidden";
 	// 		} else {
 	// 			document.body.style.overflowX = "auto";
 	// 		}
-	// 	});
+	// 	};
+	
+	// 	document.body.style.maxWidth = "1920px";
+	// 	document.body.style.overflowX = "hidden";
+	// 	window.addEventListener("resize", resizeHandler);
+	
 	// 	return () => {
-	// 		window.removeEventListener("resize", () => {
-	// 			if (window.innerWidth === 1920 && window.innerHeight === 1080) {
-	// 				document.body.style.overflowX = "hidden";
-	// 			} else {
-	// 				document.body.style.overflowX = "auto";
-	// 			}
-	// 		});
+	// 		window.removeEventListener("resize", resizeHandler);
 	// 		document.body.style.overflowX = "auto";
 	// 		document.body.style.overflowY = "auto";
 	// 	};
 	// }, []);
 
+	useEffect(() => {
+		document.body.style.overflowX = "hidden";
+
+		return() => {
+			document.body.style.overflowX = "auto";
+		};
+	}, [])
+
 	return (
-		<div className="flex flex-col gap-8 relative w-full items-center">
+		<div className="flex flex-col gap-8 relative items-center">
 			<HeaderTitlePage text="members" />
 			<div className="flex justify-center">
 				<MemberFilter
@@ -244,25 +249,25 @@ const Members: React.FC = () => {
 				<MemberList members={filteredMember} itemsPerPage={10} />
 			</div>
 			{/* Left Eclipses Decoration  */}
-			<div className="absolute top-1/2 left-0 invisible 2xl:visible transform -translate-x-12">
-				<div className="absolute w-24 h-24 rounded-full bg-ft-primary-blue-500 transform -translate-x-3/4"></div>
-				<div className="absolute w-24 h-24 rounded-full bg-ft-primary-blue-500 transform -translate-x-1/3 -translate-y-36"></div>
-				<div className="absolute w-24 h-24 rounded-full bg-[#c9d6ea] transform -translate-x-24 -translate-y-44"></div>
-				<div className="absolute w-4 h-4 rounded-full bg-ft-primary-blue-500 transform -translate-x-2/3 translate-y-36"></div>
+			<div className="absolute top-1/2 left-0 invisible xl:visible transform xl:-translate-x-24 2xl:-translate-x-52">
+				<div className="absolute w-16 h-16 2xl:w-24 2xl:h-24 rounded-full bg-ft-primary-blue-500 transform xl:-translate-x-1/3 2xl:-translate-x-3/4"></div>
+				<div className="absolute w-16 h-16 2xl:w-24 2xl:h-24 rounded-full bg-ft-primary-blue-500 transform xl:-translate-y-20 2xl:-translate-x-1/3 2xl:-translate-y-36"></div>
+				<div className="absolute w-16 h-16 2xl:w-24 2xl:h-24 rounded-full bg-[#c9d6ea] transform xl:-translate-x-2/3 xl:-translate-y-28 2xl:-translate-x-24 2xl:-translate-y-44"></div>
+				<div className="absolute w-3 h-3 2xl:w-4 2xl:h-4 rounded-full bg-ft-primary-blue-500 transform xl:translate-x-full xl:translate-y-24 2xl:-translate-x-2/3 2xl:translate-y-36"></div>
 			</div>
 			{/* Below Eclipses Decoration  */}
-			<div className="absolute bottom-0 right-0 invisible 2xl:visible transform translate-y-16 -translate-x-24">
-				<div className="absolute w-24 h-24 rounded-full bg-ft-primary-yellow-500 transform translate-x-44"></div>
-				<div className="w-24 h-24 rounded-full bg-ft-primary-blue-500 transform -translate-x-12"></div>
-				<div className="absolute w-24 h-24 rounded-full bg-ft-primary-blue-500 transform translate-x-12 translate-y-4"></div>
-				<div className="absolute w-24 h-24 rounded-full bg-[#c9d6ea] transform translate-x-32 -translate-y-4"></div>
-				<div className="absolute w-4 h-4 rounded-full bg-ft-primary-blue-500 transform -translate-x-32 -translate-y-20"></div>
-				<div className="absolute w-4 h-4 rounded-full bg-ft-primary-yellow-500 transform translate-x-52 -translate-y-64"></div>
+			<div className="absolute bottom-0 right-0 invisible xl:visible transform xl:-translate-y-8 xl:-translate-x-1/2 2xl:translate-y-16 2xl:translate-x-16">
+				<div className="absolute w-16 h-16 2xl:w-24 2xl:h-24 rounded-full bg-ft-primary-yellow-500 transform xl:translate-x-24 xl:-translate-y-6 2xl:translate-x-44"></div>
+				<div className="w-16 h-16 2xl:w-24 2xl:h-24 rounded-full bg-ft-primary-blue-500 transform xl:-translate-x-12 xl:-translate-y-6 2xl:-translate-x-12"></div>
+				<div className="absolute w-16 h-16 2xl:w-24 2xl:h-24 rounded-full bg-ft-primary-blue-500 transform xl:translate-x-6 xl:-translate-y-4 2xl:translate-x-12 2xl:translate-y-4"></div>
+				<div className="absolute w-16 h-16 2xl:w-24 2xl:h-24 rounded-full bg-[#c9d6ea] transform xl:translate-x-16 xl:-translate-y-8 2xl:translate-x-32 2xl:-translate-y-4"></div>
+				<div className="absolute w-3 h-3 2xl:w-4 2xl:h-4 rounded-full bg-ft-primary-blue-500 transform xl:-translate-x-24 xl:-translate-y-16 2xl:-translate-x-24 2xl:-translate-y-20"></div>
+				<div className="absolute w-3 h-3 2xl:w-4 2xl:h-4 rounded-full bg-ft-primary-yellow-500 transform xl:translate-x-32 xl:-translate-y-44 2xl:translate-x-52 2xl:-translate-y-64"></div>
 			</div>
 			{/* Right FinTech Bear Decoration */}
-			<img src="./FinTechBear.svg" alt="FinTech Bear" className="absolute right-0 top-1/2 rotate-[-40deg] transform -translate-y-1/2 translate-x-3/4 w-[400px] h-[400px] invisible 2xl:visible" />
+			<img src="./FinTechBear.svg" alt="FinTech Bear" className="absolute right-0 top-1/2 rotate-[-40deg] transform xl:translate-x-64 xl:-translate-y-1/2 2xl:-translate-y-1/2 2xl:translate-x-[30rem] w-[300px] h-[300px] 2xl:w-[400px] 2xl:h-[400px] invisible xl:visible" />
 			{/* Below FinTech Bear Decoration  */}
-			<img src="./FinTechBear.svg" alt="FinTech Bear" className="absolute bottom-0 left-0 w-[400px] h-[400px] translate-y-full invisible 2xl:visible" />
+			<img src="./FinTechBear.svg" alt="FinTech Bear" className="absolute bottom-0 left-0 w-[300px] h-[300px] 2xl:w-[400px] 2xl:h-[400px] xl:-translate-x-8 xl:translate-y-1/2 2xl:-translate-x-1/2 2xl:translate-y-full invisible xl:visible" />
 		</div>
 	);
 };
