80a0f60f019a94dfa3f9b40aa164b4be3cca01ea
GuitarHero2004 <anhminhnts2004@gmail.com>
2024-04-23 00:17:48 +0700

    fix codes to avoid repetition

--- app/about/(components)/headerTitlePage.tsx
+++ app/about/(components)/headerTitlePage.tsx
@@ -4,20 +4,20 @@ type HeaderTitlePageProps = {
 
 const HeaderTitlePage: React.FC<HeaderTitlePageProps> = ({ text }) => {
 	return (
-		<section className="flex justify-center my-4 items-center w-full">
+		<section className="flex justify-center my-4 mx-4 items-center w-full">
 			{/* Left arrow*/}
-			<div className="relative flex items-center">
-				<div className="bg-ft-primary-blue-500 h-1 w-64 invisible xl:visible"></div>
-				<div className="absolute bg-ft-primary-blue-500 h-4 w-4 rounded-full invisible xl:visible"></div>
+			<div className="relative flex items-center invisible lg:visible">
+				<div className="bg-ft-primary-blue-500 h-1 w-64"></div>
+				<div className="absolute bg-ft-primary-blue-500 h-4 w-4 rounded-full"></div>
 			</div>
 			{/* Title page  */}
-			<div className="bg-ft-primary-yellow-500 py-4 px-48 mx-16 rounded-2xl">
+			<div className="bg-ft-primary-yellow-500 py-4 px-24 mx-8 xl:px-48 xl:mx-16 rounded-2xl">
 				<h5 className="uppercase text-ft-text-bright">{text}</h5>
 			</div>
 			{/* Right arrow*/}
-			<div className="relative flex items-center">
-				<div className="bg-ft-primary-blue-500 h-1 w-64 invisible xl:visible"></div>
-				<div className="bg-ft-primary-blue-500 h-4 w-4 rounded-full invisible xl:visible"></div>
+			<div className="relative flex items-center invisible lg:visible">
+				<div className="bg-ft-primary-blue-500 h-1 w-64"></div>
+				<div className="bg-ft-primary-blue-500 h-4 w-4 rounded-full"></div>
 			</div>
 		</section>
 	);
