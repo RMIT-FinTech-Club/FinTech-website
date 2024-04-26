46ea85197ae3617c557a449f5c4d577d84ff5a98
Hankaji <hankaji135@gmail.com>
2024-04-26 14:55:37 +0700

    Added animation

--- components/heroVideoSection.tsx
+++ components/heroVideoSection.tsx
@@ -1,11 +1,30 @@
 import { IconChevronCompactDown } from '@tabler/icons-react'
+import { AnimationControls, TargetAndTransition, motion, type Variants } from 'framer-motion'
 import clsx from 'clsx'
 import React from 'react'
 
 
 const HerosectionVid: React.FC = () => {
+
+    const textAppearVariants: Variants = {
+        hidden: {
+            opacity: 0, y: -50,
+            transition: {
+                when: 'afterChildren',
+            }
+        },
+        visible: {
+            opacity: 1, y: 0,
+            transition: {
+                when: 'beforeChildren',
+                duration: 0.5
+            }
+        }
+    }
+
     return (
         <div className={clsx('relative w-screen h-screen')}>
+            {/* Video background */}
             <video autoPlay muted loop id='hero-section-bg-vid'
                 className={clsx('w-full h-full', // Full screen
                     'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2', // Center the video
@@ -13,12 +32,28 @@ const HerosectionVid: React.FC = () => {
                 )}>
                 <source src='/hero-section-bg-vid.mp4' type='video/mp4' />
             </video>
-            <div className='absolute bottom-16 right-0 left-0'>
-                <h2 className='text-center w-full text-ft-text-bright'>Make Fintech great again</h2>
+
+            {/* Text */}
+            <motion.div
+                initial='hidden'
+                animate='visible'
+                transition={{
+                    staggerChildren: 0.1
+                }}
+                className='absolute bottom-16 right-0 left-0'>
+                    
+                <motion.h2
+                    className='text-center w-full text-ft-text-bright'
+                    variants={textAppearVariants}
+                >Welcome to FinTech</motion.h2>
                 <br />
-                <p className='text-center w-full text-ft-text-bright'>April 1st 2025, Report</p>
-                <IconChevronCompactDown size={48} className='text-center w-full text-ft-text-bright' />
-            </div>
+
+                <motion.p className='text-center w-full text-ft-text-bright' variants={textAppearVariants}>April 1st 2025, Report</motion.p>
+
+                <motion.div variants={textAppearVariants}>
+                    <IconChevronCompactDown size={48} className='text-center w-full text-ft-text-bright' />
+                </motion.div>
+            </motion.div>
         </div>
     )
 }
