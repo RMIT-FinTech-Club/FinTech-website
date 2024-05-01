465cb6d0267d430432025916fc458a87bf75c438
GuitarHero2004 <anhminhnts2004@gmail.com>
2024-05-01 22:12:15 +0700

    add responsive design for enabling users with Iphone 13 Mini view screen

--- app/events/(components)/eventDetail.tsx
+++ app/events/(components)/eventDetail.tsx
@@ -1,20 +1,34 @@
 const EventDetail = () => {
-    return (
-        <div className="items-center w-1/2 2xl:w-1/2 h-full mx-8 my-4 2xl:mx-16 justify-between">
-            <div className="my-1 py-2 2xl:my-2 2xl:py-4">
-                <h4 className="py-1 2xl:py-2">Description</h4>
-                <p className="py-1 2xl:py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales ante eu libero iaculis, sit amet vestibulum</p>
-            </div>
-            <div className="my-1 py-2 2xl:my-2 2xl:py-4">
-                <h4 className="py-1 2xl:py-2">Time</h4>
-                <p className="py-1 2xl:py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales ante eu libero iaculis, sit amet vestibulum</p>
-            </div>
-            <div className="my-1 py-2 2xl:my-2 2xl:py-4">
-                <h4 className="py-1 2xl:py-2">Link</h4>
-                <p className="py-1 2xl:py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales ante eu libero iaculis, sit amet vestibulum</p>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div className="items-center lg:w-1/2 2xl:w-1/3 h-full mx-4 2xl:mx-16 my-4 2xl:my-8 justify-between">
+			<div className="my-1 py-1 2xl:my-2 2xl:py-4">
+				<h4 className="py-1 2xl:py-2 text-center lg:text-left">
+					Description
+				</h4>
+				<p className="py-1 2xl:py-2 text-justify">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Vestibulum sodales ante eu libero iaculis, sit amet
+					vestibulum
+				</p>
+			</div>
+			<div className="my-1 py-1 2xl:my-2 2xl:py-4">
+				<h4 className="py-1 2xl:py-2 text-center lg:text-left">Time</h4>
+				<p className="py-1 2xl:py-2 text-justify">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Vestibulum sodales ante eu libero iaculis, sit amet
+					vestibulum
+				</p>
+			</div>
+			<div className="my-1 py-1 2xl:my-2 2xl:py-4">
+				<h4 className="py-1 2xl:py-2 text-center lg:text-left">Link</h4>
+				<p className="py-1 2xl:py-2 text-justify">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Vestibulum sodales ante eu libero iaculis, sit amet
+					vestibulum
+				</p>
+			</div>
+		</div>
+	);
+};
 
 export default EventDetail;
--- app/events/eventDateAndLocation.tsx
+++ app/events/eventDateAndLocation.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import Image from 'next/image';
-import EventDetail from './(components)/eventDetail';
-import EventLocation from './(components)/eventLocation';
+import Image from "next/image";
+import React from "react";
+import EventDetail from "./(components)/eventDetail";
+import EventLocation from "./(components)/eventLocation";
 
 const EventDateAndLocation = () => {
-    return (
-        <div className='flex flex-col my-16'>
-            <div className='flex'>
-                <EventDetail />
-                <EventLocation />
-            </div>
-            <div className='absolute top-1/4 right-0'>
-                <Image
-                    loading='lazy'
-                    src='./SideFinTechBearForEventDateAndLocation.svg'
-                    alt='above decoration for event date and location'
-                    width={300}
-                    height={300}
-                />
-            </div>
-            <div className='flex justify-center my-4'>
-                {/* Below Decoration for Event Date and Location */}
-                <Image
-                    loading='lazy'
-                    src='./ThreeBearsAndDecorationsForEventDateAndLocation.svg'
-                    alt='below decoration for event date and location'
-                    width={1200}
-                    height={300}
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className="flex flex-col my-16">
+			<div className="lg:flex">
+				<EventDetail />
+				<EventLocation />
+			</div>
+			<div className="hidden lg:flex lg:absolute lg:top-1/4 lg:right-0">
+				<Image
+					loading="lazy"
+					src="./SideFinTechBearForEventDateAndLocation.svg"
+					alt="above decoration for event date and location"
+					width={300}
+					height={300}
+				/>
+			</div>
+			<div className="hidden lg:flex lg:justify-center lg:my-4">
+				{/* Below Decoration for Event Date and Location */}
+				<Image
+					loading="lazy"
+					src="./ThreeBearsAndDecorationsForEventDateAndLocation.svg"
+					alt="below decoration for event date and location"
+					width={1200}
+					height={300}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default EventDateAndLocation;
