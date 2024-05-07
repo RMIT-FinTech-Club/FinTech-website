64cbd787fdbfc62dbcb9655be143141d46d6cfb9
Hankaji <hankaji135@gmail.com>
2024-05-07 23:08:57 +0700

    Removed homePage folder cuz wtf

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,23 @@ import React from "react";
 // import IntroSection from "../components/intro";
 // import UpcomingEvent from "../components/upcomingEvent";
 import Events from "./events/page";
+import PastPartners from "./components/pastPartners";
+import HerosectionVid from "./components/heroVideoSection";
+import HeroSection from "./components/hero";
+import IntroSection from "./components/intro";
+import Individual from "./components/hall-of-fame/individual";
+import Department from "./components/department";
+import UpcomingEvent from "./components/upcomingEvent";
 const Home = () => {
 	return (
 		<div className="flex min-h-screen flex-col justify-center gap-4">
-			{/* <HerosectionVid /> */}
-			{/* <HeroSection /> */}
+			<HerosectionVid />
+			<HeroSection />
 			<PastPartners />
-			{/* <IntroSection /> */}
-			{/* <Department /> */}
-			{/* <Individual /> */}
-			{/* <UpcomingEvent /> */}
+			<IntroSection />
+			<Department />
+			<Individual />
+			<UpcomingEvent />
 		</div>
 	);
 };
