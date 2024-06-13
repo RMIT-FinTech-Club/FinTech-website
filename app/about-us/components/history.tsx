ecfd7532f2865038779b6f19846e0c3984c0567a
Logan <phamphuocsang5850@gmail.com>
2024-06-13 19:51:09 +0700

    change layout of the fintech mascot into grid instead of absolute

--- app/about-us/components/history.tsx
+++ app/about-us/components/history.tsx
@@ -73,81 +73,85 @@ export default function HistorySection() {
 					Phasellus sollicitudin
 				</div>
 			</div>
-			<div
-				className={`mt-[93px] mb-[77px] w-[500px] h-[300px] md: mx-auto ${
-					centerSlideIndex === 0
+			<div className="flex justify-between items-center w-full mt-8">
+				<img
+					alt="fintech mascot"
+					className="md:w-[300px] lg:w-[600px] rotate-[58.03deg] rounded-[59px] z-10"
+					src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=1713240956080"
+				/>
+				<div
+					className={`mt-[93px] mb-[77px] w-[500px] h-[300px] grid grid-cols-3 z-0 md: mx-auto ${centerSlideIndex === 0
 						? "pr-96"
 						: centerSlideIndex === 4
 							? "pl-96"
 							: "px-96"
-				}`}
-				ref={emblaRef}
-			>
-				<div className="flex justify-between items-center">
-					{["2020", "2021", "2022", "2023", "2024"].map(
-						(year, index) => {
-							return (
-								<div
-									key={year}
-									className={`flex shrink-0 md:ml-[60px] lg:ml-[90px] items-center rounded-full duration-1000 ease-out select-none ${
-										index === centerSlideIndex
+						}`}
+					ref={emblaRef}
+				>
+					<div className="flex justify-between items-center">
+						{["2020", "2021", "2022", "2023", "2024"].map(
+							(year, index) => {
+								return (
+									<div
+										key={year}
+										className={`flex shrink-0 md:ml-[60px] lg:ml-[90px] items-center rounded-full duration-1000 ease-out select-none ${index === centerSlideIndex
 											? "md:w-[200px] md:h-[200px] lg:w-64 lg:h-64 bg-slate-700 "
 											: "lg:w-32 lg:h-32 bg-amber-200 md:w-[120px] md:h-[120px]"
-									} `}
-								>
-									<div
-										className={`mx-auto font-semibold ${
-											index === centerSlideIndex
+											} `}
+									>
+										<div
+											className={`mx-auto font-semibold ${index === centerSlideIndex
 												? "text-orange-300 md:text-4xl lg:text-5xl "
 												: "text-slate-700 md:text-3xl lg:text-4xl"
-										} ${fontSans.style}`}
-									>
-										{year}
+												} ${fontSans.style}`}
+										>
+											{year}
+										</div>
 									</div>
-								</div>
-							);
-						},
-					)}
+								);
+							},
+						)}
+					</div>
 				</div>
+				<img
+					alt="fintech mascot"
+					className="md:w-[400px] lg:w-[600px] rotate-[-58.03deg] rounded-[59px] z-10"
+					src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=1713240956080"
+				/>
 			</div>
-			<img
-				alt=""
-				className="absolute md:w-[300px] md:left-[-120px] md:top-[340px] lg:left-[-190.15px] lg:w-[600px] top-[257px] rotate-[58.03deg] rounded-[59px]"
-				src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=1713240956080"
-			/>
-			<img
-				alt=""
-				className="absolute md:w-[400px] md:right-[-120px] md:top-[340px] lg:right-[-190.15px] lg:w-[600px] top-[257px] rotate-[-58.03deg] rounded-[59px]"
-				src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=1713240956080"
-			/>
-			<img
-				alt=""
-				className="absolute md:bottom-[-330px] lg:bottom-0 left-0 w-[330px] h-[306px] md:w-[250px] md:h-auto"
-				src="https://ik.imagekit.io/mbrrji2rk/3164276f-e306-432f-ab36-b9a275439de7.jpg?updatedAt=1713241506012"
-			/>
-			<img
-				alt=""
-				className="absolute md:bottom-[-330px] md:w-[180px] lg:bottom-0 w-[330px] h-[306px] md:w-[250px] md:h-auto right-0 rotate-[-24.12deg]"
-				src="https://ik.imagekit.io/mbrrji2rk/3164276f-e306-432f-ab36-b9a275439de7.jpg?updatedAt=1713241506012"
-			/>
-			<div
-				className={`w-[716px] mb-[26px] mx-auto text-center text-slate-400 text-5xl font-bold ${fontSans.style}`}
-			>
-				Develop and Grow
-			</div>
-			<div
-				className={` mx-auto w-[760px] text-justify text-black text-lg font-normal ${fontSans.style} tracking-wide`}
-			>
-				Richard McClintock, a Latin professor at Hampden-Sydney College
-				in Virginia, looked up one of the more obscure Latin words,
-				consectetur, from a Lorem Ipsum passage, and going through the
-				cites of the word in classical literature, discovered the
-				undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-				1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
-				and Evil) by Cicero, written in 45 BC. This book is a treatise
-				on the theory of ethics, very popular during the Renaissance.
-				The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
-				comes from a line in section 1.10.32.
+			<div className="flex justify-between items-center w-full my-8">
+				<img
+					alt=""
+					className="w-[330px] h-[306px] md:w-[250px] md:h-auto"
+					src="https://ik.imagekit.io/mbrrji2rk/3164276f-e306-432f-ab36-b9a275439de7.jpg?updatedAt=1713241506012"
+				/>
+				<div>
+
+					<div
+						className={`w-[716px] mb-[26px] mx-auto text-center text-slate-400 text-5xl font-bold ${fontSans.style}`}
+					>
+						Develop and Grow
+					</div>
+					<div
+						className={` mx-auto w-[760px] text-justify text-black text-lg font-normal ${fontSans.style} tracking-wide`}
+					>
+						Richard McClintock, a Latin professor at Hampden-Sydney College
+						in Virginia, looked up one of the more obscure Latin words,
+						consectetur, from a Lorem Ipsum passage, and going through the
+						cites of the word in classical literature, discovered the
+						undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+						1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
+						and Evil) by Cicero, written in 45 BC. This book is a treatise
+						on the theory of ethics, very popular during the Renaissance.
+						The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
+						comes from a line in section 1.10.32.
+					</div>
+				</div>
+				<img
+					alt=""
+					className="w-[330px] h-[306px] md:w-[250px] md:h-auto rotate-[-24.12deg]"
+					src="https://ik.imagekit.io/mbrrji2rk/3164276f-e306-432f-ab36-b9a275439de7.jpg?updatedAt=1713241506012"
+				/>
 			</div>
 		</>
 	);
