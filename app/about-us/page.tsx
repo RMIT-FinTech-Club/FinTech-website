05c353c0016d7208d9fbb54fb35b02a07a44e458
Bao Ho <gbaoo.1102@gmail.com>
2024-06-17 09:45:07 +0700

    ft in the eyes of president

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,12 +2,13 @@ import CoreValue from "./components/coreValue";
 import HistorySection from "./components/history";
 import Members from "./components/members";
 import VisionMission from "./components/vision-mission/missionVision";
-
+import FinTechInTheEyes from "./components/inTheEyes";
 const AboutUs = () => {
 	return (
 		<div>
 			{/* <VisionMission /> */}
-			<HistorySection />
+			{/* <HistorySection /> */}
+			<FinTechInTheEyes/>
 			{/* <Members /> */}
 			{/* <CoreValue /> */}
 		</div>
