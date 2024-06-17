9752c90a7a813cb67e744d4617a76b7d2df4f372
Logan <phamphuocsang5850@gmail.com>
2024-06-17 11:16:08 +0700

    error at modal carousel historyPage

--- app/about-us/components/history.tsx
+++ app/about-us/components/history.tsx
@@ -2,95 +2,118 @@
 import { fontSans } from "@/config/fonts";
 import useEmblaCarousel from "embla-carousel-react";
 import { useEffect, useState } from "react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, RadioGroup, Radio } from "@nextui-org/react";
 import "../../../styles/about-us/historyPage.css";
 
 export default function HistorySection() {
-	const [emblaRef, emblaApi] = useEmblaCarousel({
+	const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+	const [emblaHorizontalRef, emblaHorizontalApi] = useEmblaCarousel({
 		loop: false,
 		dragFree: true,
 	});
+
+	const [emblaVerticalRef, emblaVerticalApi] = useEmblaCarousel({
+		loop: false,
+		dragFree: true,
+		axis: 'y',
+	});
+	const [scrollBehavior, setScrollBehavior] = useState<string>("inside");
+
 	const [centerSlideIndex, setCenterSlideIndex] = useState<number>(0);
 
 	useEffect(() => {
-		if (emblaApi) {
+		if (emblaHorizontalApi) {
 			const onSelect = () => {
-				const selectedIndex = emblaApi.selectedScrollSnap();
+				const selectedIndex = emblaHorizontalApi.selectedScrollSnap();
 				setCenterSlideIndex(selectedIndex);
 			};
 
-			emblaApi.on("select", onSelect);
+			emblaHorizontalApi.on("select", onSelect);
 			return () => {
-				emblaApi.off("select", onSelect);
+				emblaHorizontalApi.off("select", onSelect);
 			};
 		}
-	}, [emblaApi]);
+
+		if (emblaVerticalApi) {
+			const onSelect = () => {
+				const selectedIndex = emblaVerticalApi.selectedScrollSnap();
+				setCenterSlideIndex(selectedIndex);
+			};
+
+			emblaVerticalApi.on("select", onSelect);
+			return () => {
+				emblaVerticalApi.off("select", onSelect);
+			};
+		}
+	}, [emblaHorizontalApi, emblaVerticalApi]);
 
 	return (
 		<>
-			<div className="mx-auto w-full px-[52px] bg-white bg-opacity-0 flex-col grid grid-cols-8 justify-center items-center gap-4">
-				<div className="sm:w-[100px] md:w-[200px] md:left-[100px] w-[340px] h-[4px] bg-black flex items-center">
-					<div className="w-6 h-6 bg-black rounded-full" />
+			<div className="mx-auto w-full md:px-[52px] bg-white bg-opacity-0 flex-col grid grid-cols-8 justify-center items-center gap-4">
+				<div className="md:inline-flex sm:hidden md:w-96 md:h-1 bg-black flex items-center">
+					<div className="md:inline-block sm:hidden md:w-6 md:h-6 bg-black rounded-full" />
 				</div>
 
-				<div className="flex-col justify-start items-center gap-1 flex col-span-6">
+				<div className="flex-col justify-start items-center md:gap-1 flex md:col-span-6 col-span-8">
 					<div
-						className={`self-stretch text-center text-slate-700 text-2xl font-semibold ${fontSans.style} tracking-tight`}
+						className={`self-stretch text-center text-slate-700 text-2xl font-bold ${fontSans.style} tracking-tight`}
 					>
 						BACK TO TIME
 					</div>
 					<div className="self-stretch text-center">
 						<span
-							className={`text-slate-700 text-5xl font-bold ${fontSans.style}`}
+							className={`text-slate-700 md:text-5xl text-2xl font-bold ${fontSans.style}`}
 						>
 							Discover the
 						</span>
 						<span
-							className={`text-zinc-800 text-5xl font-bold ${fontSans.style}`}
+							className={`text-zinc-800 md:text-5xl text-2xl font-bold ${fontSans.style}`}
 						>
 							{" "}
 						</span>
 						<span
-							className={`text-orange-300 text-5xl font-bold ${fontSans.style} `}
+							className={`text-orange-300 md:text-5xl text-2xl font-bold ${fontSans.style} `}
 						>
 							FINTECH CLUB
 							<br />
 						</span>
 						<span
-							className={`text-slate-400 text-5xl font-bold ${fontSans.style}`}
+							className={`text-slate-400 md:text-5xl text-2xl font-bold ${fontSans.style}`}
 						>
 							Story & History
 						</span>
 					</div>
 
 					<div
-						className={`mx-auto mt-[16px] text-black text-lg ${fontSans.style} tracking-wide text-center`}
+						className={`mx-auto mt-[16px] md:px-0 px-20 text-black text-lg ${fontSans.style} tracking-wide text-center`}
 					>
-						{" "}
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 						Cras viverra viverra eros, eu euismod arcu dignissim
 						quis. Phasellus sollicitudin
 					</div>
 				</div>
 
-				<div className=" sm:w-[100px] md:w-[200px] md:right-[100px] w-[340px] h-[4px] bg-black flex flex-row-reverse items-center justify-self-end">
-					<div className="w-6 h-6 bg-black rounded-full" />
+				<div className="md:inline-flex sm:hidden md:w-96 md:h-1 bg-black flex flex-row-reverse items-center justify-self-end">
+					<div className="md:inline-block sm:hidden md:w-6 md:h-6 bg-black rounded-full" />
 				</div>
 			</div>
-			<div className="items-center w-full mt-8 grid grid-cols-6">
+
+			{/* desktop version */}
+			<div className="hidden items-center w-full mt-8 md:grid md:grid-cols-6 grid-cols-1">
 				<img
 					alt="fintech mascot"
-					className="md:w-[300px] lg:w-[600px] rotate-[58.03deg] rounded-[59px] z-10"
+					className="md:block hidden md:w-[300px] lg:w-[600px] rotate-[58.03deg] rounded-[59px] z-10"
 					src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=1713240956080"
 				/>
 				<div
-					className={`mt-[93px] mb-[77px] w-[500px] h-[300px] grid grid-cols-3 col-span-4 z-0 md: mx-auto ${
-						centerSlideIndex === 0
-							? "pr-96"
-							: centerSlideIndex === 4
-								? "pl-96"
-								: "px-96"
-					}`}
-					ref={emblaRef}
+					className={`mt-[93px] mb-[77px] md:w-[0px] w-auto md:h-[300px] h-[100px] grid grid-cols-3 col-span-4 z-0 md: mx-auto ${centerSlideIndex === 0
+						? "pr-96"
+						: centerSlideIndex === 4
+							? "pl-96"
+							: "px-96"
+						}`}
+					ref={emblaHorizontalRef}
 				>
 					<div className="flex justify-between items-center">
 						{["2020", "2021", "2022", "2023", "2024"].map(
@@ -98,18 +121,16 @@ export default function HistorySection() {
 								return (
 									<div
 										key={year}
-										className={`flex shrink-0 md:ml-[50px] lg:ml-[90px] items-center rounded-full duration-1000 ease-out select-none ${
-											index === centerSlideIndex
-												? "md:w-[170px] md:h-[170px] lg:w-60 lg:h-60 bg-slate-700 "
-												: "lg:w-32 lg:h-32 bg-amber-200 md:w-[120px] md:h-[120px]"
-										} `}
+										className={`flex shrink-0 md:ml-[50px] lg:ml-[90px] items-center rounded-full duration-1000 ease-out select-none ${index === centerSlideIndex
+											? "md:w-[170px] md:h-[170px] lg:w-60 lg:h-60 bg-slate-700 w-36 h-36"
+											: "sm:inline-flex hidden lg:w-32 lg:h-32 bg-amber-200 md:w-[120px] md:h-[120px]"
+											} `}
 									>
 										<div
-											className={`mx-auto font-semibold ${
-												index === centerSlideIndex
-													? "text-orange-300 md:text-4xl lg:text-5xl "
-													: "text-slate-700 md:text-3xl lg:text-4xl"
-											} ${fontSans.style}`}
+											className={`mx-auto font-semibold ${index === centerSlideIndex
+												? "text-orange-300 md:text-4xl lg:text-5xl text-3xl"
+												: "sm:inline-flex hidden text-slate-700 md:text-3xl lg:text-4xl"
+												} ${fontSans.style}`}
 										>
 											{year}
 										</div>
@@ -121,24 +142,108 @@ export default function HistorySection() {
 				</div>
 				<img
 					alt="fintech mascot"
-					className="md:w-[400px] lg:w-[600px] rotate-[-58.03deg] rounded-[59px] z-10 "
+					className=" md:block hidden md:w-[400px] lg:w-[600px] rotate-[-58.03deg] rounded-[59px] z-10 "
 					src="https://ik.imagekit.io/mbrrji2rk/Picture1.png?updatedAt=1713240956080"
 				/>
 			</div>
-			<div className="flex justify-between items-center w-full my-8">
+
+			{/* mobile version */}
+			<div className="md:hidden items-center w-full grid grid-cols-3 overflow-x-hidden">
 				<img
 					alt=""
-					className="w-[330px] h-[306px] md:w-[250px] md:h-auto rotate-[-24.12deg]"
+					className="w-[250px] md:hidden h-auto transform -scale-x-100 rotate-[-24.12deg]"
 					src="https://ik.imagekit.io/mbrrji2rk/3164276f-e306-432f-ab36-b9a275439de7.jpg?updatedAt=1713241506012"
 				/>
-				<div>
+				<div
+					className={`mt-[93px] mb-[77px] md:w-[0px] w-auto md:h-[300px] h-[100px] md:mx-auto`}
+				>
+					<div className="flex w-auto justify-between items-center">
+						<div
+							className={`flex shrink-0 md:ml-[50px] lg:ml-[90px] items-center rounded-full duration-1000 ease-out select-none md:w-[170px] md:h-[170px] lg:w-60 lg:h-60 bg-slate-700 w-36 h-36`}
+						>
+							<div
+								className={`mx-auto font-semibold text-orange-300 md:text-4xl lg:text-5xl text-3xl ${fontSans.style}`}
+							>
+								2022
+							</div>
+						</div>
+					</div>
+				</div>
+				<img
+					alt=""
+					className="w-[250px] md:hidden h-auto transform rotate-[24.12deg] justify-self-end"
+					src="https://ik.imagekit.io/mbrrji2rk/3164276f-e306-432f-ab36-b9a275439de7.jpg?updatedAt=1713241506012"
+				/>
+			</div>
+
+			<div className="md:hidden mx-auto text-center font-semibold text-2xl text-[#5E5E92]" onClick={onOpen}>
+				change
+			</div>
+
+			<div className="flex flex-col gap-2">
+				<Modal
+					isOpen={isOpen}
+					onOpenChange={onOpenChange}
+				>
+					<ModalContent>
+						{(onClose: any) => (
+							<>
+								<ModalBody>
+									<div
+										className={`mt-[93px] mb-[77px] md:w-[0px] w-auto md:h-[300px] h-[100px] grid grid-rows-5 col-span-4 z-0 md:mx-auto ${centerSlideIndex === 0
+											? "pr-96"
+											: centerSlideIndex === 4
+												? "pl-96"
+												: "px-96"
+											}`}
+										ref={emblaVerticalRef}
+									>
+										<div className="flex flex-col justify-between items-center gap-8">
+											{["2020", "2021", "2022", "2023", "2024"].map(
+												(year, index) => {
+													return (
+														<div
+															key={year}
+															className={`flex shrink-0 md:ml-[50px] lg:ml-[90px] items-center rounded-full duration-1000 ease-out select-none ${index === centerSlideIndex
+																? "md:w-[170px] md:h-[170px] lg:w-60 lg:h-60 bg-slate-700 w-36 h-36"
+																: "sm:inline-flex w-24 h-24 bg-amber-200"
+																} `}
+														>
+															<div
+																className={`mx-auto font-semibold ${index === centerSlideIndex
+																	? "text-orange-300 md:text-4xl lg:text-5xl text-3xl"
+																	: "sm:inline-flex text-slate-700 text-3xl lg:text-4xl"
+																	} ${fontSans.style}`}
+															>
+																{year}
+															</div>
+														</div>
+													);
+												},
+											)}
+										</div>
+									</div>
+								</ModalBody>
+							</>
+						)}
+					</ModalContent>
+				</Modal>
+			</div>
+
+			<div className="grid grid-cols-3 justify-between items-center w-full my-8">
+				<img
+					alt=""
+					className="md:w-[250px] hidden md:inline-block md:h-auto rotate-[-24.12deg]"
+					src="https://ik.imagekit.io/mbrrji2rk/3164276f-e306-432f-ab36-b9a275439de7.jpg?updatedAt=1713241506012"
+				/>
+				<div className="md:col-span-1 col-span-full">
 					<div
-						className={`w-[716px] mb-[26px] mx-auto text-center text-slate-400 text-5xl font-bold ${fontSans.style}`}
+						className={`md:mb-[26px] mb-2 mx-auto text-center text-slate-400 md:text-5xl text-3xl md:px-0 font-semibold ${fontSans.style}`}
 					>
 						Develop and Grow
 					</div>
 					<div
-						className={` mx-auto w-[760px] text-justify text-black text-lg font-normal ${fontSans.style} tracking-wide`}
+						className={`mx-auto text-justify text-black text-lg font-medium ${fontSans.style} tracking-wide md:p-0 px-14`}
 					>
 						Richard McClintock, a Latin professor at Hampden-Sydney
 						College in Virginia, looked up one of the more obscure
@@ -156,7 +261,7 @@ export default function HistorySection() {
 				</div>
 				<img
 					alt=""
-					className="w-[330px] h-[306px] md:w-[250px] md:h-auto transform -scale-x-100 rotate-[24.12deg]"
+					className="md:w-[250px] md:inline-block hidden md:h-auto transform -scale-x-100 rotate-[24.12deg] justify-self-end"
 					src="https://ik.imagekit.io/mbrrji2rk/3164276f-e306-432f-ab36-b9a275439de7.jpg?updatedAt=1713241506012"
 				/>
 			</div>
--- app/cms/researchForm.tsx
+++ app/cms/researchForm.tsx
@@ -140,22 +140,55 @@ export default function PodcastForm() {
 					</div>
 
 					<div className="w-full row-span-2 p-3 rounded-lg border-2 border-gray-300 border-dashed flex-col justify-center items-center gap-3 inline-flex cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-						<label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full rounded-lg ">
+						<label
+							htmlFor="dropzone-file"
+							className="flex flex-col items-center justify-center w-full rounded-lg "
+						>
 							<div className="flex flex-col items-center justify-center pt-2 pb-6">
-								<svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+								<svg
+									className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 20 16"
+								>
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+									/>
 								</svg>
-								<p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold text-[#DCB968]">Click</span> to browse your computer or drag pdf file to this area </p>
-								<p className="text-xs text-gray-500 dark:text-gray-400">(Max. File size: 25MB)</p>
+								<p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+									<span className="font-semibold text-[#DCB968]">
+										Click
+									</span>{" "}
+									to browse your computer or drag pdf file to
+									this area{" "}
+								</p>
+								<p className="text-xs text-gray-500 dark:text-gray-400">
+									(Max. File size: 25MB)
+								</p>
 							</div>
-							<input id="dropzone-file" type="file" className="hidden" />
+							<input
+								id="dropzone-file"
+								type="file"
+								className="hidden"
+							/>
 						</label>
 					</div>
 					<div className="flex items-end gap-2 justify-end border-t border-zinc-300 pt-6">
-						<button className="w-1/3 h-10 rounded-lg text-gray-500 font-semibold text-lg tracking-wide border border-zinc-300 hover:bg-gray-100 active:bg-gray-200" type="reset">
+						<button
+							className="w-1/3 h-10 rounded-lg text-gray-500 font-semibold text-lg tracking-wide border border-zinc-300 hover:bg-gray-100 active:bg-gray-200"
+							type="reset"
+						>
 							Cancel
 						</button>
-						<button className="w-1/3 h-10 rounded-lg bg-[#DCB968] hover:bg-[#DCB968]/80 active:bg-[#DCB968]/60 text-gray-500 font-semibold text-lg tracking-wide" type="submit">
+						<button
+							className="w-1/3 h-10 rounded-lg bg-[#DCB968] hover:bg-[#DCB968]/80 active:bg-[#DCB968]/60 text-gray-500 font-semibold text-lg tracking-wide"
+							type="submit"
+						>
 							Save
 						</button>
 					</div>
