a5b0f5be4e047e905fb7bba4ebc9594f9d91191b
Minh <s3979367@rmit.edu.vn>
2024-06-19 23:58:09 +0700

    finished button layout and background for mobile UI

--- app/join-us/components/roadToFtcButton.tsx
+++ app/join-us/components/roadToFtcButton.tsx
@@ -8,9 +8,20 @@ interface RoadToFtcButtonProps {
 
 const RoadToFtcButton = ({ text, color }: RoadToFtcButtonProps) => {
     return (
-        <Button className={`mx-8 my-2 py-2 px-4 ${color} text-white font-bold rounded-xl w-96 h-24 text-4xl`}>
-            {text}
-        </Button>
+        <div>
+            {/* Laptop view */}
+            <div className="hidden lg:flex">
+                <Button className={`mx-8 my-2 py-2 px-4 ${color} text-white font-bold rounded-xl w-96 h-24 text-4xl`}>
+                    {text}
+                </Button>
+            </div>
+            {/* Mobile view */}
+            <div className="flex lg:hidden">
+                <Button className={`mx-2 my-2 py-2 px-2 ${color} text-white font-bold rounded-full w-44 h-12 text-xl`}>
+                    {text}
+                </Button>
+            </div>
+        </div>
     );
 };
 
