be29de21f023e702c73d25691fbf8c7d8472f32f
Minh <s3979367@rmit.edu.vn>
2024-06-22 15:32:46 +0700

    adjust the size of the p tags

--- app/join-us/components/roadToFtcContainer.tsx
+++ app/join-us/components/roadToFtcContainer.tsx
@@ -16,7 +16,7 @@ const RoadToFtcContainer: React.FC<RoadToFtcContainerProps> = ({
 			<div className="rounded-2xl bg-gradient-to-r from-ft-primary-blue-200 to-ft-primary-blue-500 p-1 lg:p-2 w-4/5 lg:w-full">
 				<div className="flex flex-col items-center px-2 py-1 lg:px-4 lg:py-2 rounded-[calc(1rem-0.25rem)] bg-white w-full">
 					<h5 className="px-1 py-1 lg:py-2 lg:px-2">{header}</h5>
-					<p className="px-1 py-1 lg:py-2 lg:px-2 text-center text-sm lg:text-base leading-normal w-4/5">
+					<p className="px-1 py-1 lg:py-2 lg:px-2 text-center leading-normal w-4/5">
 						{text}
 					</p>
 				</div>
