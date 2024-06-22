f09311cb8ea872ca131a3bca180d4273bb48430b
StarkDaiDe <141554508+StarkDaiDe@users.noreply.github.com>
2024-06-22 09:46:11 +0700

    update check biome

--- app/api/v1/auth/login/route.ts
+++ app/api/v1/auth/login/route.ts
@@ -1,9 +1,9 @@
 import { connect } from "@/db/dbConfig";
 import User from "@/models/user";
 import bcryptjs from "bcryptjs";
+import { SignJWT } from "jose";
 // import jwt from "jsonwebtoken";
 import { type NextRequest, NextResponse } from "next/server";
-import { SignJWT } from "jose";
 
 export async function POST(request: NextRequest) {
 	try {
@@ -29,12 +29,15 @@ export async function POST(request: NextRequest) {
 		// Create JWT token
 		const jwtSecretKey = process.env.JWT_SECRET;
 		const secretKey = new TextEncoder().encode(jwtSecretKey);
-		const token = await new SignJWT(
-			{ id: user._id, email: user.email, role: user.role }
-		)    .setProtectedHeader({ alg: "HS256" })
-		.setIssuedAt()
-		.setExpirationTime("3 days")
-		.sign(secretKey);
+		const token = await new SignJWT({
+			id: user._id,
+			email: user.email,
+			role: user.role,
+		})
+			.setProtectedHeader({ alg: "HS256" })
+			.setIssuedAt()
+			.setExpirationTime("3 days")
+			.sign(secretKey);
 		// Create response
 		const response = NextResponse.json(
 			{ message: "Login successfully!" },
