24b78f7d44bb3481dca89da7450b297ec2f7ebb9
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-06-24 17:08:43 +0700

    Add media project page

---
+++ app/projects/media-projects/page.tsx
@@ -0,0 +1,20 @@
+import Layout from './components/Layout';
+import React from 'react';
+import Header from './components/Header';
+
+const projects = [
+	{ title: 'Trung thu', description: 'Description for Trung thu', year: 2022 },
+	{ title: 'Tết Nguyên Đán', description: 'Description for Tết Nguyên Đán', year: 2023 },
+	{ title: 'Giáng sinh Christio', description: 'Description for Giáng sinh Christio', year: 2021 },
+	// Add other projects similarly
+];
+
+const Home = () => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+export default Home;
