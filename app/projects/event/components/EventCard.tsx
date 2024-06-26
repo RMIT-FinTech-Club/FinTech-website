65b2d1647c28b005a5210c96e47cebefc7913688
Logan <phamphuocsang5850@gmail.com>
2024-06-26 22:36:05 +0700

    finish project events

--- app/projects/event/components/EventCard.tsx
+++ app/projects/event/components/EventCard.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useRef, useState } from "react";
-import Slider from "react-slick";
+import { IconArrowsMaximize } from '@tabler/icons-react';
 
 const EventCard = () => {
     return (
@@ -8,27 +7,99 @@ const EventCard = () => {
                 <h1 className="text-center text-[#dbb969]">
                     Past Events
                 </h1>
-                <p className="mx-auto mt-12 md:px-10 px-2 text-lg tracking-wide text-center">
+                <p className="mx-auto md:mt-12 mt-6 md:px-10 px-9 text-lg tracking-wide text-center">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, velit? Laborum commodi mollitia facere perspiciatis facilis, debitis delectus nobis ipsam cumque esse quas officiis, odio possimus error, numquam vel eaque.
                 </p>
-                <div className="grid grid-cols-3 grid-flow-row mt-12 gap-14 px-40">
-                    <div className="w-full justify-self-center m-auto items-center text-center bg-[#dbb969] h-52 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
-                        Event 1
+                <div className="grid md:grid-cols-3 md:grid-flow-row mt-12 gap-14 md:px-40 px-12">
+                    <div className="w-full grid grid-rows-2 justify-self-center m-auto items-center text-center bg-[#dbb969] h-64 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
+                        <div className='row-start-2 row-span-1 md:px-10 px-4 grid md:grid-cols-12 grid-cols-4'>
+                            <div className='w-fit h-fit md:col-span-2 col-span-1 bg-black hover:bg-black/90 rounded-2xl p-2 hover:p-4 transition-all duration-3000'>
+                                <IconArrowsMaximize className='w-12 h-12' stroke={2} color='white' />
+                            </div>
+                            <div className='ml-5 md:col-span-10 col-span-3 justify-self-start text-left'>
+                                <div className='text-4xl font-extrabold'>
+                                    2019
+                                </div>
+                                <div className='md:text-xl text-lg'>
+                                    Lorem ipsum dolor sit
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="w-full justify-self-center m-auto items-center text-center bg-[#dbb969] h-52 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
-                        Event 1
+                    <div className="w-full grid grid-rows-2 justify-self-center m-auto items-center text-center bg-[#dbb969] h-64 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
+                        <div className='row-start-2 row-span-1 md:px-10 px-4 grid md:grid-cols-12 grid-cols-4'>
+                            <div className='w-fit h-fit md:col-span-2 col-span-1 bg-black hover:bg-black/90 rounded-2xl p-2 hover:p-4 transition-all duration-3000'>
+                                <IconArrowsMaximize className='w-12 h-12' stroke={2} color='white' />
+                            </div>
+                            <div className='ml-5 md:col-span-10 col-span-3 justify-self-start text-left'>
+                                <div className='text-4xl font-extrabold'>
+                                    2020
+                                </div>
+                                <div className='md:text-xl text-lg'>
+                                    Lorem ipsum dolor sit
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="w-full justify-self-center m-auto items-center text-center bg-[#dbb969] h-52 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
-                        Event 1
+                    <div className="w-full grid grid-rows-2 justify-self-center m-auto items-center text-center bg-[#dbb969] h-64 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
+                        <div className='row-start-2 row-span-1 md:px-10 px-4 grid md:grid-cols-12 grid-cols-4'>
+                            <div className='w-fit h-fit md:col-span-2 col-span-1 bg-black hover:bg-black/90 rounded-2xl p-2 hover:p-4 transition-all duration-3000'>
+                                <IconArrowsMaximize className='w-12 h-12' stroke={2} color='white' />
+                            </div>
+                            <div className='ml-5 md:col-span-10 col-span-3 justify-self-start text-left'>
+                                <div className='text-4xl font-extrabold'>
+                                    2021
+                                </div>
+                                <div className='md:text-xl text-lg'>
+                                    Lorem ipsum dolor sit
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="w-full justify-self-center m-auto items-center text-center bg-[#dbb969] h-52 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
-                        Event 1
+                    <div className="w-full grid grid-rows-2 justify-self-center m-auto items-center text-center bg-[#dbb969] h-64 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
+                        <div className='row-start-2 row-span-1 md:px-10 px-4 grid md:grid-cols-12 grid-cols-4'>
+                            <div className='w-fit h-fit md:col-span-2 col-span-1 bg-black hover:bg-black/90 rounded-2xl p-2 hover:p-4 transition-all duration-3000'>
+                                <IconArrowsMaximize className='w-12 h-12' stroke={2} color='white' />
+                            </div>
+                            <div className='ml-5 md:col-span-10 col-span-3 justify-self-start text-left'>
+                                <div className='text-4xl font-extrabold'>
+                                    2022
+                                </div>
+                                <div className='md:text-xl text-lg'>
+                                    Lorem ipsum dolor sit
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="w-full justify-self-center m-auto items-center text-center bg-[#dbb969] h-52 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
-                        Event 1
+                    <div className="w-full grid grid-rows-2 justify-self-center m-auto items-center text-center bg-[#dbb969] h-64 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
+                        <div className='row-start-2 row-span-1 md:px-10 px-4 grid md:grid-cols-12 grid-cols-4'>
+                            <div className='w-fit h-fit md:col-span-2 col-span-1 bg-black hover:bg-black/90 rounded-2xl p-2 hover:p-4 transition-all duration-3000'>
+                                <IconArrowsMaximize className='w-12 h-12' stroke={2} color='white' />
+                            </div>
+                            <div className='ml-5 md:col-span-10 col-span-3 justify-self-start text-left'>
+                                <div className='text-4xl font-extrabold'>
+                                    2023
+                                </div>
+                                <div className='md:text-xl text-lg'>
+                                    Lorem ipsum dolor sit
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="w-full justify-self-center m-auto items-center text-center bg-[#dbb969] h-52 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
-                        Event 1
+                    <div className="w-full grid grid-rows-2 justify-self-center m-auto items-center text-center bg-[#dbb969] h-64 rounded-2xl hover:bg-[#dbb969]/90 hover:shadow-2xl transition-all duration-3000">
+                        <div className='row-start-2 row-span-1 md:px-10 px-4 grid md:grid-cols-12 grid-cols-4'>
+                            <div className='w-fit h-fit md:col-span-2 col-span-1 bg-black hover:bg-black/90 rounded-2xl p-2 hover:p-4 transition-all duration-3000'>
+                                <IconArrowsMaximize className='w-12 h-12' stroke={2} color='white' />
+                            </div>
+                            <div className='ml-5 md:col-span-10 col-span-3 justify-self-start text-left'>
+                                <div className='text-4xl font-extrabold'>
+                                    2024
+                                </div>
+                                <div className='md:text-xl text-lg'>
+                                    Lorem ipsum dolor sit
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
