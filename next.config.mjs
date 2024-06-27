0461eb6e2772d8ff49b7dd495e6ba2c9cc8465af
TanNguyen17 <150835094+TanNguyen17@users.noreply.github.com>
2024-06-27 14:04:12 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['axios','mongoose'],
-    },
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        config.externals.push({
-            'utf-8-validate': 'commonjs utf-8-validate',
-            'bufferutil': 'commonjs bufferutil',
-            'supports-color': 'commonjs supports-color',
-        })
-        return config
-    },
-}
-export default nextConfig
+const nextConfig = {};
+
+export default nextConfig;
